929669b4e511d5ba14949cb2e01f70c412da021b
Chathura <cpriydasun@gmail.com>
2024-02-09 03:11:40 +0530

    View Registration is Front end Updated

--- client/src/components/System/StudentRecords/Record.jsx
+++ client/src/components/System/StudentRecords/Record.jsx
@@ -2,7 +2,7 @@
 import React, { useEffect, useState, useCallback } from "react";
 import RecordStyles from "./Record.module.css";
 import SystemSideBar from "../SystemSideBar/SystemSideBar";
-import { Form, Input, Button, DatePicker, message, Select } from "antd";
+import { Form, Input, Button, Checkbox, message } from "antd";
 import { CloseSquareOutlined } from "@ant-design/icons";
 import { useLocation, useNavigate } from "react-router-dom";
 import axios from "axios";
@@ -24,10 +24,27 @@ const Record = () => {
   const [newMobileNumber, setNewMobileNumber] = useState("");
   const [newSchool, setNewSchool] = useState("");
   const [newBirthday, setNewBirthday] = useState("");
+  const [whatsAppNumber, setWhatsAppNumber] = useState("");
+  const [grade, setGrade] = useState("");
+  const [cambrige, setCambrige] = useState("");
+  const [elocution, setElocution] = useState("");
+  const [general, setGeneral] = useState("");
+  const [nameOfFather, setNameOfFather] = useState("");
+  const [fathersOccupation, setFathersOccupation] = useState("");
+  const [fathersContactNumber, setFathersContactNumber] = useState("");
+  const [fathersEmail, setFathersEmail] = useState("");
+  const [nameOfMother, setNameOfMother] = useState("");
+  const [mothersOccupation, setMothersOccupation] = useState("");
+  const [mothersContactNumber, setMothersContactNumber] = useState("");
+  const [mothersEmail, setMothersEmail] = useState("");
+  const [nameOfGuardian, setNameOfGuardian] = useState("");
+  const [GuardianOccupation, setGuardianOccupation] = useState("");
+  const [GuardianContactNumber, setGuardianContactNumber] = useState("");
+  const [Guardian, setGuardian] = useState("");
+  
 
   const [form] = Form.useForm();
   const navigate = useNavigate();
-
 
   console.log(location);
 
@@ -40,86 +57,88 @@ const Record = () => {
     school: "",
     birthday: "",
   });
-  
-const [loading, setLoading] = useState(true);
 
-const getOneUserRecords = async () => {
-  try {
-    const id = location.state.id;
-    console.log(id);
-    const response = await axios.post("http://localhost:8080/api/v1/registration/get-only-one-user-details",{ id: id });
-    console.log(response);
+  const [loading, setLoading] = useState(true);
 
-    // const date = new Date(response.data.details.birthday);
-    // const formattedDate = date.toISOString().split("T")[0];
-
-    // setFormValues({
-    //   fullName: response.data.details.fullName,
-    //   indexNumber: response.data.details.indexNumber,
-    //   nameWithInitials: response.data.details.nameWithInitials,
-    //   mobileNumber: response.data.details.mobileNumber,
-    //   school: response.data.details.school,
-    //   address: response.data.details.address,
-    //   birthday: formattedDate, // Use formattedDate here
-      
-    // });
-
-    setLoading(false); // Add this line
-  } catch (error) {
-    message.error("Student Data fetched unsuccessfull.please Try again later.");
-    setLoading(false); // Add this line
-  }
-};
-
-
-const handleDelete = async (id) => {
-  try {
-
-    if (window.confirm("Are you sure you want to delete the data?")) {
-
+  const getOneUserRecords = async () => {
+    try {
+      const id = location.state.id;
       console.log(id);
-      const response = await axios.post("http://localhost:8080/api/v1/delete/delete-route", { id })
-      console.log(response.data);
+      const response = await axios.post(
+        "http://localhost:8080/api/v1/registration/get-only-one-user-details",
+        { id: id }
+      );
+      console.log(response);
 
-      if (response.data.success) {
-        message.success("Data deleted successfully");
-        window.location.reload();
-      }
-    } else {
-      message.info("Deletion canceled by user");
+      // const date = new Date(response.data.details.birthday);
+      // const formattedDate = date.toISOString().split("T")[0];
+
+      // setFormValues({
+      //   fullName: response.data.details.fullName,
+      //   indexNumber: response.data.details.indexNumber,
+      //   nameWithInitials: response.data.details.nameWithInitials,
+      //   mobileNumber: response.data.details.mobileNumber,
+      //   school: response.data.details.school,
+      //   address: response.data.details.address,
+      //   birthday: formattedDate, // Use formattedDate here
+
+      // });
+
+      setLoading(false); // Add this line
+    } catch (error) {
+      message.error(
+        "Student Data fetched unsuccessfull.please Try again later."
+      );
+      setLoading(false); // Add this line
     }
+  };
 
+  const handleDelete = async (id) => {
+    try {
+      if (window.confirm("Are you sure you want to delete the data?")) {
+        console.log(id);
+        const response = await axios.post(
+          "http://localhost:8080/api/v1/delete/delete-route",
+          { id }
+        );
+        console.log(response.data);
 
-  } catch (error) {
-    message.error("Error deleting data");
+        if (response.data.success) {
+          message.success("Data deleted successfully");
+          window.location.reload();
+        }
+      } else {
+        message.info("Deletion canceled by user");
+      }
+    } catch (error) {
+      message.error("Error deleting data");
+    }
+  };
+
+  const handleUpdate = async (values) => {
+    try {
+      // update-student-record
+      const id = location.state.id;
+      console.log(values);
+
+      const response = await axios.post(
+        "http://localhost:8080/api/v1/registration/update-student-record",
+        { id: id, values }
+      );
+      navigate("/records");
+      message.success("Student Updated Successfully.");
+    } catch (error) {
+      message.error("Student Updated Unsuccessful.please Try again later.");
+    }
+  };
+
+  useEffect(() => {
+    getOneUserRecords();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
   }
-}
-
-
-const handleUpdate = async(values)=>{
-   try {
-// update-student-record
-    const id = location.state.id;
-    console.log(values);
-
-   const response = await axios.post("http://localhost:8080/api/v1/registration/update-student-record",{id:id,values})
-   navigate("/records")
-   message.success("Student Updated Successfully.");
-   } 
-
-   catch (error) {
-    message.error("Student Updated Unsuccessful.please Try again later.");
-   }
-}
-
-
-useEffect(() => {
-  getOneUserRecords();
-}, []);
-
-if (loading) {
-  return <div>Loading...</div>;
-}
   return (
     <div>
       <SystemSideBar>
@@ -231,6 +250,7 @@ if (loading) {
                   />
                 </Form.Item>
               </div>
+
               <div
                 style={{
                   display: "flex",
@@ -251,6 +271,7 @@ if (loading) {
                   />
                 </Form.Item>
               </div>
+
               <div
                 style={{
                   display: "flex",
@@ -258,7 +279,30 @@ if (loading) {
                   alignItems: "center",
                 }}
               >
-                <label className={RecordStyles.RegFormLabel}>Birthday:</label>
+                <label className={RecordStyles.RegFormLabel}>
+                  WhatsApp Number:
+                </label>
+
+                <Form.Item name="whatsAppNumber" style={{ flex: "2" }}>
+                  <Input
+                    value={whatsAppNumber}
+                    onChange={(e) =>
+                      setWhatsAppNumber(e.target.value || whatsAppNumber)
+                    }
+                  />
+                </Form.Item>
+              </div>
+
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <label className={RecordStyles.RegFormLabel}>
+                  Date of Birth:
+                </label>
                 <Form.Item name="birthday" style={{ flex: "2" }}>
                   <Input
                     value={birthday}
@@ -266,6 +310,7 @@ if (loading) {
                   />
                 </Form.Item>
               </div>
+
               <div
                 style={{
                   display: "flex",
@@ -285,6 +330,24 @@ if (loading) {
                   />
                 </Form.Item>
               </div>
+
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <label className={RecordStyles.RegFormLabel}>Grade:</label>
+
+                <Form.Item name="grade" style={{ flex: "2" }}>
+                  <Input
+                    value={grade}
+                    onChange={(e) => setGrade(e.target.value || grade)}
+                  />
+                </Form.Item>
+              </div>
+
               <div
                 style={{
                   display: "flex",
@@ -293,13 +356,78 @@ if (loading) {
                 }}
               >
                 <label className={RecordStyles.RegFormLabel}>
-                  Current British English Course:
+                  Examination:
                 </label>
-                <Form.Item name="address" style={{ flex: "2" }}>
-                  <TextArea
-                    rows={4}
-                    value={address}
-                    onChange={(e) => setNewAddress(e.target.value || address)}
+
+                <Form.Item name="examination" style={{ flex: "2" }}>
+                  <Checkbox.Group
+                    className={`${RecordStyles.Special} ${RecordStyles.SpecialAreaFix}`}
+                    name="examination"
+                    id="examination"
+                  >
+                    <Checkbox
+                      className="myCheckbox"
+                      value="Cambridge Assessment"
+                      onChange={(e) => setCambrige(e.target.value)}
+                    >
+                      Cambridge Assessments
+                    </Checkbox>
+                    <Checkbox
+                      className="myCheckbox"
+                      value="Elocution Exams"
+                      onChange={(e) => setElocution(e.target.value)}
+                    >
+                      Elocution Exams
+                    </Checkbox>
+                    <Checkbox
+                      className="myCheckbox"
+                      value="General English"
+                      onChange={(e) => setGeneral(e.target.value)}
+                    >
+                      General English
+                    </Checkbox>
+                  </Checkbox.Group>
+                </Form.Item>
+              </div>
+
+              <div className={RecordStyles.RegHeadersTop}>
+                {"Enter Parents' / Guardian's Details:"}
+              </div>
+
+              <div className={RecordStyles.RegHeaders}>
+                {"Details of the Father"}
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <label className={RecordStyles.RegFormLabel}>Name:</label>
+
+                <Form.Item name="nameOfFather" style={{ flex: "2" }}>
+                  <Input
+                    value={nameOfFather}
+                    onChange={(e) => setNameOfFather(e.target.value || grade)}
+                  />
+                </Form.Item>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <label className={RecordStyles.RegFormLabel}>Occupation:</label>
+
+                <Form.Item name="fathersOccupation" style={{ flex: "2" }}>
+                  <Input
+                    value={fathersOccupation}
+                    onChange={(e) =>
+                      setFathersOccupation(e.target.value || grade)
+                    }
                   />
                 </Form.Item>
               </div>
@@ -311,28 +439,184 @@ if (loading) {
                 }}
               >
                 <label className={RecordStyles.RegFormLabel}>
-                  Completed British English Courses:
+                  Contact Number:
                 </label>
-                <Form.Item name="address" style={{ flex: "2" }}>
-                  <TextArea
-                    rows={4}
-                    value={address}
-                    onChange={(e) => setNewAddress(e.target.value || address)}
+
+                <Form.Item name="fathersContactNumber" style={{ flex: "2" }}>
+                  <Input
+                    value={fathersContactNumber}
+                    onChange={(e) =>
+                      setFathersContactNumber(e.target.value || grade)
+                    }
                   />
                 </Form.Item>
               </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <label className={RecordStyles.RegFormLabel}>Email:</label>
+
+                <Form.Item name="fathersEmail" style={{ flex: "2" }}>
+                  <Input
+                    value={fathersEmail}
+                    onChange={(e) => setFathersEmail(e.target.value || grade)}
+                  />
+                </Form.Item>
+              </div>
+
+              <div className={RecordStyles.RegHeaders}>
+                {"Details of the Mother"}
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <label className={RecordStyles.RegFormLabel}>Name:</label>
+
+                <Form.Item name="nameOfMother" style={{ flex: "2" }}>
+                  <Input
+                    value={nameOfMother}
+                    onChange={(e) => setNameOfMother(e.target.value || grade)}
+                  />
+                </Form.Item>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <label className={RecordStyles.RegFormLabel}>Occupation:</label>
+
+                <Form.Item name="mothersOccupation" style={{ flex: "2" }}>
+                  <Input
+                    value={mothersOccupation}
+                    onChange={(e) =>
+                      setMothersOccupation(e.target.value || grade)
+                    }
+                  />
+                </Form.Item>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <label className={RecordStyles.RegFormLabel}>
+                  Contact Number:
+                </label>
+
+                <Form.Item name="mothersContactNumber" style={{ flex: "2" }}>
+                  <Input
+                    value={mothersContactNumber}
+                    onChange={(e) =>
+                      setMothersContactNumber(e.target.value || grade)
+                    }
+                  />
+                </Form.Item>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <label className={RecordStyles.RegFormLabel}>Email:</label>
+
+                <Form.Item name="mothersEmail" style={{ flex: "2" }}>
+                  <Input
+                    value={mothersEmail}
+                    onChange={(e) => setMothersEmail(e.target.value || grade)}
+                  />
+                </Form.Item>
+              </div>
+
+              <div className={RecordStyles.RegHeaders}>
+                {"Details of the Guardian"}
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <label className={RecordStyles.RegFormLabel}>Name:</label>
+
+                <Form.Item name="nameOfGuardian" style={{ flex: "2" }}>
+                  <Input
+                    value={nameOfGuardian}
+                    onChange={(e) => setNameOfGuardian(e.target.value || grade)}
+                  />
+                </Form.Item>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <label className={RecordStyles.RegFormLabel}>Occupation:</label>
+
+                <Form.Item name="GuardianOccupation" style={{ flex: "2" }}>
+                  <Input
+                    value={GuardianOccupation}
+                    onChange={(e) =>
+                      setGuardianOccupation(e.target.value || grade)
+                    }
+                  />
+                </Form.Item>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <label className={RecordStyles.RegFormLabel}>
+                  Contact Number:
+                </label>
+
+                <Form.Item name="GuardianContactNumber" style={{ flex: "2" }}>
+                  <Input
+                    value={GuardianContactNumber}
+                    onChange={(e) =>
+                      setGuardianContactNumber(e.target.value || grade)
+                    }
+                  />
+                </Form.Item>
+              </div>
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "row",
+                  alignItems: "center",
+                }}
+              >
+                <label className={RecordStyles.RegFormLabel}>Email:</label>
+
+                <Form.Item name="Guardian" style={{ flex: "2" }}>
+                  <Input
+                    value={Guardian}
+                    onChange={(e) => setGuardian(e.target.value || grade)}
+                  />
+                </Form.Item>
+              </div>
+
               <div className={RecordStyles.buttonGroup}>
-                {/* <Button
-                  type="submit"
-                  onClick={() => handleUpdate(formValues)}
-                  style={{
-                    color: "#73d13d",
-                    border: "1px solid #73d13d",
-                    width: "150px",
-                  }}
-                >
-                  Update Record
-                </Button> */}
                 <Button
                   type="ghost"
                   htmlType="submit"
